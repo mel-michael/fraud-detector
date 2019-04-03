cd052ef4648b6ee3cca90319a29ecaa728d6b499
Mel Michael <mel.michael007@gmail.com>
2019-04-03 11:06:38 +0100

    Boostrap with a starter template

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -6,7 +6,7 @@ export const ENV = {
 
 let config = {
     environment: process.env.NODE_ENV || ENV.dev,
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 5000,
     logging: true
 };
 
