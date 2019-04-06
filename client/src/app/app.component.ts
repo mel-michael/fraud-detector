5609d47a32f0d90f85c7d10a6071cf35b7b54b61
Mel Michael <mel.michael@andela.com>
2019-04-06 16:57:38 +0100

    Implement search feature

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormControl, FormGroup } from '@angular/forms';
 import { AgGridNg2 } from 'ag-grid-angular';
-// import { IconDefinition } from '@fortawesome/fontawesome-common-types'
-// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 import DATA from '../transaction-graph';
 
@@ -14,11 +12,9 @@ import DATA from '../transaction-graph';
 })
 
 export class AppComponent implements OnInit {
-  @ViewChild('agGrid') agGrid: AgGridNg2;
-  title = 'My App';
-  searchValue: any;
+  @ViewChild('agGrid')agGrid: AgGridNg2;
+  tempData: any;
   rowData: any;
-  // faCoffee: IconDefinition = faCoffee;
 
   appForm = new FormGroup({
     transactionId: new FormControl(''),
@@ -26,32 +22,46 @@ export class AppComponent implements OnInit {
   });
 
   columnDefs = [
-    { headerName: 'S/N', width: 50,
-      cellRenderer: function(params) {
+    {
+      headerName: 'S/N',
+      width: 50,
+      cellRenderer: function (params) {
         return params.rowIndex + 1;
       }
-    },
-    { headerName: 'Name', field: 'name' },
-    { headerName: 'Email', field: 'email' },
-    { headerName: 'Phone', field: 'phone' },
-    { headerName: 'Age', field: 'age', width: 80 },
-    { headerName: 'Transaction ID', field: 'id' },
-    { headerName: 'Connection Type',
-      cellRenderer: function(params) {
+    }, {
+      headerName: 'Name',
+      field: 'name'
+    }, {
+      headerName: 'Email',
+      field: 'email'
+    }, {
+      headerName: 'Phone',
+      field: 'phone'
+    }, {
+      headerName: 'Age',
+      field: 'age',
+      width: 80
+    }, {
+      headerName: 'Transaction ID',
+      field: 'id'
+    }, {
+      headerName: 'Connection Type',
+      cellRenderer: function (params) {
         if (params.data.connectionInfo) {
           return params.data.connectionInfo.type;
         }
         return '';
       }
-    },
-    { headerName: 'Confidence Level', filter: 'agNumberColumnFilter',
-      cellRenderer: function(params) {
+    }, {
+      headerName: 'Confidence Level',
+      filter: 'agNumberColumnFilter',
+      cellRenderer: function (params) {
         if (params.data.connectionInfo) {
           return params.data.connectionInfo.confidence;
         }
         return '';
       }
-    },
+    }
   ];
 
   gridOptions = {
@@ -64,67 +74,73 @@ export class AppComponent implements OnInit {
     colWidth: 100,
     rowSelection: 'single',
     onGridReady: function (event) {
-      event.api.sizeColumnsToFit();
-    },
+      event
+        .api
+        .sizeColumnsToFit();
+    }, // TODO: Use this if Ag-Grid is activated
     // isExternalFilterPresent: this.isExternalFilterPresent,
     // doesExternalFilterPass: this.doesExternalFilterPass,
   };
 
+  constructor(private http: HttpClient) {}
 
-  isExternalFilterPresent() {
-    // if searchValue is not empty, then we are filtering
-    return this.searchValue !== '';
-  }
-
-  doesExternalFilterPass(node) {
-    console.log('node', node);
-    // const { transactionId, confidenceLevel } = this.searchValue;
-    // console.log()
-    return true;
-
-    // switch (this.searchValue) {
-    //   case 'below30': return node.data.age < 30;
-    //   case 'between30and50': return node.data.age >= 30 && node.data.age <= 50;
-    //   case 'above50': return node.data.age > 50;
-    //   default: return true;
-    // }
-  }
-
-  constructor(private http: HttpClient) { }
-
-  // Flatten the data set
+  // flatten the data set
   flatten(items) {
     let flat = [];
     items.forEach((el) => {
       if (el.childrens === undefined || el.childrens.length === 0) {
         return flat.push(el);
       }
-      // flat.push(el)
+      // flat.push(el); // TODO: Only add this if we need the root data
       flat = flat.concat(this.flatten(el.childrens));
     });
     return flat;
   }
 
-  externalFilterChanged() {
-    this.searchValue = this.appForm.value;
-    console.log(this.searchValue);
-    this.agGrid.api.onFilterChanged();
-    // this.agGrid.api.setQuickFilter(`${transactionId} ${confidenceLevel}`);
+  searchBy() {
+    const {transactionId, confidenceLevel} = this.appForm.value;
+
+    if (transactionId === '' && confidenceLevel === '') {
+      this.rowData = this.tempData;
+      return null;
+    }
+
+    // filter by user input
+    this.rowData = this.tempData
+      .filter(data => {
+        if (data.connectionInfo) {
+          // match both transaction id and confidence level
+          if (transactionId && confidenceLevel) {
+            if (data.id === transactionId && data.connectionInfo.confidence > Number(confidenceLevel)) {
+              return data;
+            }
+          }
+          // match only the transaction id
+          if (transactionId && !confidenceLevel && data.id === transactionId) {
+            return data;
+          }
+          // match only confidence level
+          if (confidenceLevel && !transactionId && data.connectionInfo.confidence > Number(confidenceLevel)) {
+            return data;
+          }
+        }
+        // match only transaction data that doesn't have a confidence level
+        if (data.id === transactionId) {
+          return data;
+        }
+      });
   }
 
-  onSearch() {
-    this.searchValue = this.appForm.value;
-    console.log(this.searchValue);
-    this.agGrid.api.onFilterChanged();
-    // this.agGrid.api.setQuickFilter(`${transactionId} ${confidenceLevel}`);
+  reset(event) {
+    event.preventDefault();
+    this.appForm.reset();
+    this.rowData = this.tempData;
   }
 
   ngOnInit() {
-    // this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
-    // this.rowData = DATA;
-    this.rowData = this.flatten(DATA);
-    console.log(DATA);
+    const transactionData = this.flatten(DATA);
+    this.rowData = transactionData;
+    this.tempData = transactionData; // Sort temp data for client-side filtering
   }
 
 }
-
